4ed023d32035ac9d19d012c6090739b7b7299798
Ivan Fadhila <ivan.fadhila70@gmail.com>
2019-02-04 14:08:15 +0700

    init

--- hello.js
+++ hello.js
@@ -1,8 +1,10 @@
-var express = require('express')
-var app = express()
+let express = require('express')
+let app = express()
+
  
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
- 
+
 app.listen(3000)
+console.log('Server is running on http://localhost:3000/!');
